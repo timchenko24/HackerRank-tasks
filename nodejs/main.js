fceea62c652841dc3e26f0c2735cd187f6c3b93d
Tim Chenskiy <timadraft@gmail.com>
2020-07-12 19:54:42 +0300

    Drawing Book

--- nodejs/main.js
+++ nodejs/main.js
@@ -14,5 +14,14 @@ function sockMerchant(ar) {
     return pairs;
 }
 
+//https://www.hackerrank.com/challenges/drawing-book/problem
+function pageCount(n, p) {
+    let numberOfTurns = (n - 1) % 2 === 1 ? Math.floor((n - 1) / 2) + 1 : (n - 1) / 2;
+    let numberOfTurnsFromStart = (p - 1) % 2 === 1 ? Math.floor((p - 1) / 2) + 1 : (p - 1) / 2;
+    let numberOfTurnsFromEnd = numberOfTurns - numberOfTurnsFromStart;
+
+    return numberOfTurnsFromStart > numberOfTurnsFromEnd ? numberOfTurnsFromEnd : numberOfTurnsFromStart;
+}
+
 const arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-console.log(sockMerchant(arr))
+console.log(pageCount(7, 2))
